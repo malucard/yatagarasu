c30a5aece511fd1176b8d5f263bc2f8bc4605a5e
PydraxAlpta <49099987+PydraxAlpta@users.noreply.github.com>
2022-06-08 10:38:38 +0530

    added mystery list add and remove commands

--- src/commands/mysteryfiction/poll-list-commands.ts
+++ src/commands/mysteryfiction/poll-list-commands.ts
@@ -1,6 +1,6 @@
 import { ChannelTypes } from "discord.js/typings/enums";
 import { CombinedApplicationCommand } from "../../bot";
-import Discord from "discord.js";
+import Discord, { Interaction } from "discord.js";
 import { hiddenReply } from "../../utils/helpers";
 
 const FLAGS = Discord.Permissions.FLAGS;
@@ -102,39 +102,35 @@ export const MF_Commands: CombinedApplicationCommand[] = [
 	{
 		name: "mysterylist",
 		description: "Commands for the mysterylist",
-		options: [
-			{
-				name: "add",
-				type: "SUB_COMMAND",
-				description: "Add an entry to the mystery list",
-				options: [
-					{
-						name: "name",
-						description: "Name of the game to add to the list",
-						type: "STRING",
-						required: true
-					}
-				]
+		options: [{
+			name: "action",
+			description: "Add or Remove from the Mystery List",
+			type: "STRING",
+			choices: [{
+				name: "Add Game",
+				value: "add"
 			}, {
-				name: "remove",
-				type: "SUB_COMMAND",
-				description: "Remove an entry from the mystery list",
-				options: [
-					{
-						name: "name",
-						description: "Name of the game to remove from the list",
-						type: "STRING",
-						required: true,
-						autocomplete: true
-					}
-				]
-			}
-		],
+				name: "Remove Game",
+				value: "remove"
+			}],
+			required: true
+		}, {
+			name: "name",
+			description: "Name of the Game",
+			type: "STRING",
+			required: true
+		}, {
+			name: "do_it",
+			description: "Actually perform the action, otherwise by default replies with how it would look.",
+			type: "BOOLEAN",
+			required: false
+		}],
 		action: async interaction => {
 			const guild = interaction.guild;
 			const me = guild.me;
 			const mysteryListChannel = guild.channels.cache.find(channel => channel.name === "mystery-list");
 			const member = interaction.member as Discord.GuildMember;
+			const action = interaction.options.getString("action");
 			if (!member.permissions.has("ADMINISTRATOR")) {
 				hiddenReply(interaction, "Only admins can use these commands");
 				return;
@@ -143,65 +139,79 @@ export const MF_Commands: CombinedApplicationCommand[] = [
 				hiddenReply(interaction, "Bot does not have valid perms for mystery list channel");
 				return;
 			}
-			if (interaction.options.getSubcommand() === "add") {
-				const newGame = interaction.options.getString("name");
-				if (mysteryListChannel instanceof Discord.TextChannel) {
-					const messages = await mysteryListChannel.awaitMessages({
-						filter: message => message.author.id === me.id && message.embeds.length > 0
-					});
-					const message = messages.filter(message => message.embeds.some(embed => embed.title.startsWith("Other Games"))).first();
-					const embeds = message.embeds.filter(embed => embed.title.startsWith("Other Games"));
-
-					const games = embeds.flatMap(embed => embed.description.split("\n"));
-					games.push(newGame);
-					games.sort();
-					let join = "";
-					const descriptions: string[] = [];
-					for (let index = 0; index < games.length; index++) {
-						const game = games[index];
-						if (join.length + game.length > 4000) {
-							// I'm not touching the 4096 limit with a 100 character pole
-							descriptions.push(join);
-							join = "";
-						}
-						join = join ? game : `${join}\n${game}`;
-					}
-					descriptions.push(join);
-					const [header, spoiler, ...others] = message.embeds;
-					const tiermakers = others.at(-1);
-					const newOthers: Discord.MessageEmbed[] = descriptions.map((description, index) => new Discord.MessageEmbed({
-						title: `Other Games${index ? " (continued)" : ""}`,
-						description: description
-					}));
-					if (newOthers.length > 7) {
-						hiddenReply(interaction, "Too many embeds");
+			let name = interaction.options.getString("name");
+			if (mysteryListChannel instanceof Discord.TextChannel) {
+				const messages = (await mysteryListChannel.messages.fetch()).filter(message => message.author.id === me.id);
+				const message = messages.filter(message => message.embeds.some(embed => embed.title.startsWith("Other Games"))).first();
+				const embeds = message.embeds.filter(embed => embed.title.startsWith("Other Games"));
+				const games = embeds.flatMap(embed => embed.description.split("\n"));
+				if (action === "add") {
+					games.push(name);
+				} else {
+					const index = games.findIndex(game => game.toLowerCase() === name.toLowerCase());
+					if (index > -1) {
+						[name] = games.splice(index, 1);
+					} else {
+						hiddenReply(interaction, "Game not found.");
 						return;
 					}
-					const actuallyDoIt = false;
-					if (actuallyDoIt) {
-						// edit the embed
-						message.edit({
-							embeds: [header, spoiler, ...newOthers, tiermakers]
-						});
-						hiddenReply(interaction, `${newGame} added to ${mysteryListChannel.toString()}`);
+				}
+				games.sort();
+				const [header, spoiler, ...others] = message.embeds;
+				const tiermakers = others.at(-1);
+				const newOthers = getNewOthers(games);
+				if (newOthers.length > 7) {
+					hiddenReply(interaction, "Too many embeds");
+					return;
+				}
+				const actuallyDoIt = interaction.options.getBoolean("do_it", false);
+				if (actuallyDoIt) {
+					// edit the embed
+					message.edit({
+						embeds: [header, spoiler, ...newOthers, tiermakers]
+					});
+					if (action === "add") {
+						hiddenReply(interaction, `${name} added to ${mysteryListChannel.toString()}`);
 					} else {
-						// send the updated embed as a reply
-						if (me.permissionsIn(interaction.channel).has(BOT_PERMS)) {
-							interaction.reply({
-								content: "This is how it would look like",
-								embeds: [header, spoiler, ...newOthers, tiermakers]
-							});
-						} else {
-							hiddenReply(interaction, "Could not reply to you in this channel");
-							return;
-						}
+						hiddenReply(interaction, `${name} removed from ${mysteryListChannel.toString()}`);
+					}
+				} else {
+					// send the updated embed as a reply
+					if (me.permissionsIn(interaction.channel).has(BOT_PERMS)) {
+						interaction.reply({
+							content: "This is how it would look like",
+							embeds: [header, spoiler, ...newOthers, tiermakers],
+							ephemeral: true
+						});
+					} else {
+						hiddenReply(interaction, "Could not reply to you in this channel");
+						return;
 					}
 				}
-			} else if (interaction.options.getSubcommand() === "remove") {
-				// remove
-				hiddenReply(interaction, "Not implemented yet.");
+			} else {
+				hiddenReply(interaction, "Mystery List channel not found.");
 				return;
 			}
 		}
 	}
 ];
+
+function getNewOthers(games: string[]): Discord.MessageEmbed[] {
+	let join = "";
+	const descriptions: string[] = [];
+	for (let index = 0; index < games.length; index++) {
+		const game = games[index];
+		if (join.length + game.length > 4000) {
+			// I'm not touching the 4096 limit with a 100 character pole
+			descriptions.push(join);
+			join = "";
+		}
+		join = !join ? game : `${join}\n${game}`;
+	}
+	descriptions.push(join);
+	const newOthers: Discord.MessageEmbed[] = descriptions.map((description, index) => new Discord.MessageEmbed({
+		title: `Other Games${index > 0 ? " (continued)" : ""}`,
+		description: description
+	}));
+	return newOthers;
+}
